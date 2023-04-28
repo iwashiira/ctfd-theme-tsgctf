44207fac41c2f3cd7429598e348ef8eb346b0c9b
iwashiira <iwashiira@gmail.com>
2023-04-29 02:43:17 +0900

    update dist

--- static/OneSignalSDKWorker.js
+++ static/OneSignalSDKWorker.js
@@ -1,1 +1,1 @@
-importScripts('/themes/tsgctf/static/sw.js?1682702691757', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
+importScripts('/themes/tsgctf/static/sw.js?1682703734323', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
