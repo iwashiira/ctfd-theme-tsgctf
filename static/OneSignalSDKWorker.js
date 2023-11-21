a8ab44248598af9f851b16390e6f1d093972f57e
iwashiira <iwashiira@gmail.com>
2023-11-21 14:38:52 +0900

    TSG LIVE! 10 CTF ->  TSG LIVE! 11 CTF

--- static/OneSignalSDKWorker.js
+++ static/OneSignalSDKWorker.js
@@ -1,1 +1,1 @@
-importScripts('/themes/tsgctf/static/sw.js?1683817512286', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
+importScripts('/themes/tsgctf/static/sw.js?1700544806370', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
